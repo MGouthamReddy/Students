50af2eedce94eb47824b773e24a50121243bbfa3
MGouthamReddy <reddygoutham142@gmail.com>
2019-09-26 07:28:52 +0530

    1 file changes

---
+++ src/com/pawana/std/Student.java
@@ -0,0 +1,20 @@
+package com.pawana.std;
+
+public class Student {
+
+	private int id;
+	private String ename;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+}
